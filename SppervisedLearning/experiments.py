83c0cc11730ca255fb9f50c2fa2f431054c7f399
lshamaei3 <ladan.shamaei@gatech.edu>
2023-01-28 23:45:47 -0600

    DT experiments

--- SppervisedLearning/experiments.py
+++ SppervisedLearning/experiments.py
@@ -11,24 +11,23 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import LearningCurveDisplay, ShuffleSplit
 from sklearn.model_selection import learning_curve
 
-def prepare_data(dataset_name):
-    dataset = pd.read_csv(f'../Data/{dataset_name}')
-    y_train = dataset['class']
-    x_train = dataset.drop(labels=['class'], axis=1)
+# def prepare_data(dataset_name):
+dataset = pd.read_csv(f'../Data/mushrooms.csv')
+y_train = dataset['class']
+x_train = dataset.drop(labels=['class'], axis=1)
 
-    le = LabelEncoder()
+le = LabelEncoder()
 
-    cols = x_train.columns.values
-    for col in cols:
-        x_train[col] = le.fit_transform(x_train[col])
+cols = x_train.columns.values
+for col in cols:
+    x_train[col] = le.fit_transform(x_train[col])
 
-    y_train = le.fit_transform(y_train)
+y_train = le.fit_transform(y_train)
 
-    ohe = OneHotEncoder()
-    x_train = ohe.fit_transform(x_train).toarray()
-    sc = StandardScaler()
-    x_train = sc.fit_transform(x_train)
-    return x_train, y_train
+ohe = OneHotEncoder()
+x_train = ohe.fit_transform(x_train).toarray()
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
 
 # x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.30, random_state=42)
 # x_valid, x_test, y_valid, y_test = train_test_split(x_test, y_test, test_size=0.50, random_state=42)
@@ -44,7 +43,7 @@ def prepare_data(dataset_name):
 
 # ##Validation curve
 def prepare_val_curve(param_name,param_range,scoring, algorithm_name):
-    x_train, y_train = prepare_data()
+
     train_scores, test_scores = validation_curve(
         DecisionTreeClassifier(random_state=0),
         x_train,
@@ -90,9 +89,8 @@ def prepare_val_curve(param_name,param_range,scoring, algorithm_name):
 
 # ##Grid search
 def grid_search(parameters, scoring, refit, model=DecisionTreeClassifier(random_state=0)):
-    x_train, y_train = prepare_data()
 
-    parameters={'max_depth':np.arange(1,40,1), 'criterion':('gini','entropy','log_loss'), 'min_samples_leaf':np.arange(1,10,1)}
+
     clf = GridSearchCV(model, parameters,scoring='f1', refit='f1')
     clf.fit(x_train,y_train)
     x=(pd.DataFrame(clf.cv_results_))
@@ -101,28 +99,40 @@ def grid_search(parameters, scoring, refit, model=DecisionTreeClassifier(random_
     print(clf.best_params_)
 
 #learning curve
-train_sizes, train_scores, valid_scores = learning_curve(DecisionTreeClassifier(random_state=0), x_train, y_train, cv=5)
+def create_learning_curve(models):
+    # train_sizes, train_scores, valid_scores = learning_curve(DecisionTreeClassifier(random_state=0), x_train, y_train, cv=5)
 
-model=DecisionTreeClassifier(random_state=0)
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6), sharey=True)
+    model=DecisionTreeClassifier(random_state=0)
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6), sharey=True)
 
-common_params = {
-    "X": x_train,
-    "y": y_train,
-    "train_sizes": np.linspace(0.1, 1.0, 9),
-    "cv": ShuffleSplit(n_splits=50, test_size=0.2, random_state=0),
-    "score_type": "both",
-    "n_jobs": 4,
-    "line_kw": {"marker": "o"},
-    "std_display_style": "fill_between",
-    "score_name": "f1",
-}
+    common_params = {
+        "X": x_train,
+        "y": y_train,
+        "train_sizes": np.linspace(0.1, 1.0, 9),
+        "cv": ShuffleSplit(n_splits=50, test_size=0.2, random_state=0),
+        "score_type": "both",
+        "n_jobs": 4,
+        "line_kw": {"marker": "o"},
+        "std_display_style": "fill_between",
+        "score_name": "f1",
+    }
 
-for ax_idx, estimator in enumerate([model]):
-    LearningCurveDisplay.from_estimator(estimator, **common_params, ax=ax[ax_idx])
-    handles, label = ax[ax_idx].get_legend_handles_labels()
-    ax[ax_idx].legend(handles[:2], ["Training Score", "Test Score"])
-    ax[ax_idx].set_title(f"Learning Curve for {estimator.__class__.__name__}")
-plt.show()
+    for ax_idx, estimator in enumerate([model]):
+        LearningCurveDisplay.from_estimator(estimator, **common_params, ax=ax[ax_idx])
+        handles, label = ax[ax_idx].get_legend_handles_labels()
+        ax[ax_idx].legend(handles[:2], ["Training Score", "Test Score"])
+        ax[ax_idx].set_title(f"Learning Curve for {estimator.__class__.__name__}")
+    plt.show()
 
-def perform_experiments():
+if __name__=="__main__":
+    #Decision Tree
+    model=DecisionTreeClassifier(random_state=0)
+    parameters = {'max_depth': np.arange(1, 40, 1), 'criterion': ('gini', 'entropy', 'log_loss'),
+                  'min_samples_leaf': np.arange(1, 10, 1)}
+    grid_search(parameters, scoring="f1", refit="f1", model=model)
+    prepare_val_curve('max_depth',np.arange(1,20,1),"f1","DTClassifier")
+    prepare_val_curve('min_samples_leaf', np.arange(1, 10, 1), "f1", "DTClassifier")
+    create_learning_curve([model])
+
+    #ANN
+    
